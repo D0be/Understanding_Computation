b131eee99c0e8a4baf3999209fe50dd3d5a46aa8
D0be <moon562464768@sina.com>
2017-12-31 13:46:21 +0800

    NFA-ε

--- NFA.rb
+++ NFA.rb
@@ -28,6 +28,15 @@ class NFARulebook < Struct.new(:rules)
     def rules_for(state, character)
         rules.select { |rule| rule.applies_to?(state, character) }
     end
+
+    def follow_free_moves(states)
+        more_states = next_states(states, nil)
+        if more_states.subset?(states)
+            states
+        else
+            follow_free_moves(states + more_states)
+        end
+    end
 end
 
 class NFA < Struct.new(:current_states, :accept_states, :rulebook)
@@ -56,11 +65,17 @@ class NFADesign < Struct.new(:start_state, :accept_states, :rulebook)
     end
 end
 
-rulebook = NFARulebook.new([
-    FARule.new(1, 'a', 1), FARule.new(1, 'b', 1), FARule.new(1, 'b', 2),
-    FARule.new(2, 'a', 3), FARule.new(2, 'b', 3),
-    FARule.new(3, 'a', 4), FARule.new(3, 'b', 4)
-])
+class NFA
+    def current_states
+        rulebook.follow_free_moves(super)
+    end
+end
+
+# rulebook = NFARulebook.new([
+#     FARule.new(1, 'a', 1), FARule.new(1, 'b', 1), FARule.new(1, 'b', 2),
+#     FARule.new(2, 'a', 3), FARule.new(2, 'b', 3),
+#     FARule.new(3, 'a', 4), FARule.new(3, 'b', 4)
+# ])
 
 # p rulebook.next_states(Set[1], 'b')                 # => #<Set: {1, 2}>
 # p rulebook.next_states(Set[1, 2], 'a')              # => #<Set: {1, 3}>
@@ -82,7 +97,26 @@ rulebook = NFARulebook.new([
 # nfa.read_string('bbbbb')
 # p nfa.accepting?                                          # => true
 
-nfa_design = NFADesign.new(1, [4], rulebook)
-p nfa_design.accepts?('bab')                                # => true
-p nfa_design.accepts?('bbbbb')                              # => true
-p nfa_design.accepts?('bbabb')                              # => false
+# nfa_design = NFADesign.new(1, [4], rulebook)
+# p nfa_design.accepts?('bab')                                # => true
+# p nfa_design.accepts?('bbbbb')                              # => true
+# p nfa_design.accepts?('bbabb')                              # => false
+
+# rulebook = NFARulebook.new([
+#     FARule.new(1, nil, 2), FARule.new(1, nil, 4),
+#     FARule.new(2, 'a', 3),
+#     FARule.new(3, 'a', 2),
+#     FARule.new(4, 'a', 5),
+#     FARule.new(5, 'a', 6),
+#     FARule.new(6, 'a', 4)
+# ])
+
+# p rulebook.next_states(Set[1], nil)                         # => #<Set: {2, 4}>
+
+# p rulebook.follow_free_moves(Set[1])                          # => #<Set: {1, 2, 4}>
+
+# nfa_design = NFADesign.new(1, [2, 4], rulebook)
+# p nfa_design.accepts?('aa')                                     # => true
+# p nfa_design.accepts?('aaa')                                    # => true
+# p nfa_design.accepts?('aaaaa')                                  # => false
+# p nfa_design.accepts?('aaaaaa')                                 # => true
